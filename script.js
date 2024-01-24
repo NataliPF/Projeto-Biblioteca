89d944c7f11389d82a1487c5d533ac67c3c1042b
Nátali Projetti <144743079+NataliPF@users.noreply.github.com>
2024-01-24 12:03:04 -0300

    Enviando Projeto Biblioteca

---
+++ script.js
@@ -0,0 +1,92 @@
+class MaterialBibliografico {
+    constructor(titulo, autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this._disponivel = true;
+    }
+
+    realizarEmprestimo() {
+        if (this._disponivel) {
+            this._disponivel = false;
+            return true; //Emprestimo realizado com sucesso
+        } else {
+            return false; //Material já emprestado
+        }
+    }
+
+    realizarDevolucao() {
+        if(!this._disponivel) {
+            this._disponivel = true;
+            return true; //Devolução realizada com sucesso
+        } else {
+            return false; //Material já devolvido previamente
+        }
+    }
+    
+    renomearAutor(novoNome) {
+        if(novoNome == "") {
+            return alert("Não pode cadastrar nome vazio")
+        }
+        this.autor = novoNome;
+    }
+}
+
+class Livro extends MaterialBibliografico {
+    constructor(titulo, autor, genero) {
+        super(titulo, autor);
+        this.genero = genero;
+    }
+
+    alterarGenero(novoGenero) {
+        this.genero = novoGenero;
+    }
+}
+
+class Revista extends MaterialBibliografico {
+    constructor(titulo, autor, categoria) {
+        super(titulo, autor);
+        this.categoria = categoria;
+    }
+}
+
+function realizarAcao(acao) {
+    const selectLivros = document.getElementById("livros");
+    const selectedIndex = selectLivros.selectedIndex;
+
+    if(selectedIndex === 0) {
+        alert("Por favor, selecione um livro válido.");
+        return;
+    }
+
+    const livroSelecionado = livros[selectedIndex - 1];
+
+    if(acao === 'emprestimo') {
+        const emprestimoSucesso = livroSelecionado.realizarEmprestimo();
+        exibirResultado(`Emprestimo de ${livroSelecionado.titulo}: ${emprestimoSucesso? 'Sucesso' : 'Material já emprestado'}`);
+    } else if (acao === 'devolucao') {
+        const devolucaoSucesso = livroSelecionado.realizarDevolucao();
+        exibirResultado(`Devolução de ${livroSelecionado.titulo}: ${devolucaoSucesso? 'Sucesso' : 'Material já devolvido'}`);
+    }
+}
+
+function exibirResultado(mensagem) {
+    const resultadoDiv = document.getElementById("resultado");
+    resultadoDiv.innerHTML += `<p>${mensagem}</p>`
+}
+
+const livros = [
+    new Livro("O Senhor dos Anéis", "J.R.R. Tolkien", "Fantasia"),
+    new Livro("Harry Potter e a Pedra Filosofal", "J.K. Rowling", "Fantasia"),
+    new Livro("Crepúsculo", "Stephenie Meyer", "Romance"),
+    new Revista("National Geographic", "National Geographic Society", "Ciência")
+];
+
+const selectLivros = document.getElementById("livros");
+
+for(let i = 0; i < livros.length; i++) {
+    const livro = livros[i];
+    const option = document.createElement("option");
+    option.value = i + 1; //Adiciona 1 para evitar o valor 0, que representa a opção padrão
+    option.text = livro.titulo;
+    selectLivros.add(option);
+}
